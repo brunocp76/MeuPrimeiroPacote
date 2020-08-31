800a11d12dd05e23988ae5b6facd1743935214e4
Bruno Cesar Pasquini <39478173+brunocp76@users.noreply.github.com>
2020-08-31 00:32:35 -0300

    Initial commit

---
+++ R/exercicio_01.R
@@ -0,0 +1,13 @@
+#' O primeiro pacote a gente nunca esquece...
+#'
+#' Concatena duas strings com um espaco em branco entre elas
+#'
+#' @param x Primeira String
+#' @param y Segunda String
+#'
+#' @return A concatenacao das duas Strings
+#'
+#' @export
+colar <- function(x, y) {
+   return(paste(x, y, sep = " "))
+}
